74ae6163e75a63b683e58596b98a52346793a406
Mwngchao Wu <>
2020-08-12 22:39:33 +0800

    add: add moveZero,plusOne,twoSum's solution code

---
+++ Week_01/assignments_01/MoveZero.js
@@ -0,0 +1,11 @@
+var moveZeroes = function(nums) {
+  let j=0,len=nums.length;
+  for(let i=0;i<len;i++){
+      if(nums[i]!==0){
+          let temp=nums[i];
+          nums[i]=nums[j];
+          nums[j]=temp;
+          j++;
+      }
+  }
+};
---
+++ Week_01/assignments_01/PlusOne.js
@@ -0,0 +1,17 @@
+var plusOne = function(digits) {
+  let right=digits.length-1;
+  let carry=1;
+  for(let i=right;i>=0;i--){
+      digits[i]+=carry;
+      if(digits[i]===10){
+          digits[i]=0;
+          carry=1;
+      }else{
+          carry=0
+      } 
+  }
+  if(digits[0]===0){
+      digits.unshift(1);
+  }
+  return digits;
+};
