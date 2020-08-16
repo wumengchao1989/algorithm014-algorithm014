24f5896495cda1c083abadaa6a6c3847c6d17588
Mwngchao Wu <>
2020-08-16 23:16:41 +0800

    update readme

--- Week_01/assignments_01/MoveZero.js
+++ Week_01/assignments_01/MoveZero.js
@@ -11,3 +11,13 @@ var moveZeroes = function (nums) {
   }
   return nums;
 };
+
+var moveZeroes = function(nums) {
+  let len=nums.length,lastNonZeroFoundAt=0;
+  for(let i=0;i<len;i++){
+      if(nums[i]===0){
+          nums[lastNonZeroFoundAt++]=nums[i]
+          if(i!==lastNonZeroFoundAt)nums[i]=0;
+      }
+  }
+};
