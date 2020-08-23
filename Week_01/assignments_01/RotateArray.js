d38fefa1347fc4e56462bf1d2a9b2e59ec5e92d6
Mwngchao Wu <>
2020-08-23 09:35:27 +0800

    update solution of rotate array

--- Week_01/assignments_01/RotateArray.js
+++ Week_01/assignments_01/RotateArray.js
@@ -1,8 +1,18 @@
-var rotate = function (nums, k) {
-  while (k > 0) {
-    let num = nums.pop();
-    nums.unshift(num);
-    k--;
-  }
-  return nums;
+var rotate = (nums, k) => {
+  const len = nums.length;
+  k%=nums.length;
+  reverse(nums, 0, len - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, len - 1);
 };
+var reverse = (nums, start, end) => {
+  while (start < end) {
+    let temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start++;
+    end--;
+  }
+};
+
+rotate([2, 3, 4, 5, 6, 7], 3);
