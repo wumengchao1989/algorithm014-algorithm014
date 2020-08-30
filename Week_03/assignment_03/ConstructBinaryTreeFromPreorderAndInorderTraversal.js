fe457aa20a5afe042423638ef01916c1e07f5d3e
Mwngchao Wu <>
2020-08-30 22:54:44 +0800

    upd:fix spell

--- Week_03/assignment_03/ConstructBinaryTreeFromPreorderAndInorderTraversal.js
+++ Week_03/assignment_03/ConstructBinaryTreeFromPreorderAndInorderTraversal.js
@@ -1,10 +1,10 @@
 var buildTree = function(preorder, inorder) {
-  if (inorder.length === 0) return null;
-  
-  let index = inorder.indexOf( preorder[0] );
-  let root = new TreeNode( preorder.shift() );
+  if (inorder.legnth === 0) return null;
+
+  let index = inorder.indexOf(preorder[0]);
+  let root = new TreeNode(preorder.shift());
   root.left = buildTree(preorder, inorder.slice(0, index));
   root.right = buildTree(preorder, inorder.slice(index + 1));
-  
   return root;
+  
 };
